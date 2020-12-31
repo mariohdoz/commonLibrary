a6ee10ef1c317429c8a5b079754d460b81bd662d
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-12-31 14:57:15 -0300

    [ADD] configuración adicional

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number,
+  green: number,
+  blue: number
+}
+
+const color: Color = {
+  red: 1,
+  green: 2,
+  blue: 3
+}
+
+console.table(color);
+
+export default color;
